0a596c340ebaccc15ff04e329a991eb6fe9ba9fb
Teddy Brewer <tebrewer@jbi-ltd.com>
2019-08-09 00:31:04 -0500

    logic to make the game work.  clickme added, render added.

--- src/app.js
+++ src/app.js
@@ -8,4 +8,44 @@ class App extends React.Component {
         images_clicked: [],
         message: "Click an image to earn points, but don't click on the same image twice or you'll go back to 0!"
     }
+    
+    clickme = data =>{
+        let id = data.id;
+        
+        if (this.state.images_clicked.indexOf(id) === -1) {
+
+            this.setState({
+                score: this.state.score +1,
+                images_clicked: [...this.state.images_clicked, id],
+                images: this.state.images.sort(() => Math.floor(Math.random))
+            })
+
+        } else {
+
+            this.setState({
+                score: 0,
+                images_clicked: [],
+                images: this.state.images.sort(() => Math.floor(Math.random))
+            })
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <div>{this.state.mssage}</div>
+                <div>Score: {this.state.score}</div>
+
+                <Image
+                    id={import.id}
+                    alt={img.alt}
+                    src={img.src}
+                    clickme={this.clickme}
+                    key={img.id}
+                    />
+            </div>
+        )
+    }
 }
+
+export default App;
